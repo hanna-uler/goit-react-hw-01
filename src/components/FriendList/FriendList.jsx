50e900d5a803c76944e325407b159aea0d8aade9
hanna-uler <hannauler@gmail.com>
2025-04-19 07:32:47 -0400

    add FriendList files

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,10 @@
+export default function FriendList(friends) {
+    return (
+       <div>
+  <img src="" alt="Avatar" width="48" />
+  <p>Friend name</p>
+  <p>Friend status</p>
+</div>
+
+   ) 
+}
