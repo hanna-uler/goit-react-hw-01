d80aa73e13ef5f68a929aca2d8ea9475a80710fb
hanna-uler <hannauler@gmail.com>
2025-04-19 07:24:27 -0400

    remove unness const in App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,17 +1,15 @@
 import Profile from "../Profile/Profile";
 import userData from "../../userData.json";
 
-const {username, tag, location, avatar, stats} = userData;
-
 export default function App() {
   return (
     <div>
       <Profile
-        image={avatar}
-        name={username}
-        tag={tag}
-        location={location}
-        stats={stats}
+        image={userData.avatar}
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        stats={userData.stats}
       />
     </div>
   )
